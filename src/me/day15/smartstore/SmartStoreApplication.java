d1917fdb9b63d5bf2ba781494c3548aebb0414fb
Irang <irang0426@gmail.com>
2022-11-25 21:55:36 +0900

    feat: add code

--- src/me/day15/smartstore/SmartStoreApplication.java
+++ src/me/day15/smartstore/SmartStoreApplication.java
@@ -1,7 +1,16 @@
 package me.day15.smartstore;
 
+import me.day15.smartstore.customers.Customer;
+import me.day15.smartstore.customers.Customers;
+import me.day15.smartstore.groups.Group;
+import me.day15.smartstore.groups.GroupType;
+import me.day15.smartstore.groups.Groups;
+import me.day15.smartstore.groups.Parameter;
+
 public class SmartStoreApplication  { // 싱글톤
     private static SmartStoreApplication smartStoreApplication;
+    private Groups allGroups = Groups.getInstance();
+    private Customers allCustomers = Customers.getInstance();
 
     private SmartStoreApplication() {}
 
@@ -21,6 +30,16 @@ public class SmartStoreApplication  { // 싱글톤
 
 
     public SmartStoreApplication test() {
+//        allGroups.add(new Group(GroupType.GENERAL, new Parameter(10, 100000)));
+//        allGroups.add(new Group(GroupType.VIP, new Parameter(20, 200000)));
+//        allGroups.add(new Group(GroupType.VVIP, new Parameter(30, 300000)));
+
+        for (int i = 0; i < 20; i++) {
+            allCustomers.add(new Customer(
+                    Character.toString((char) ('a' + i)),
+                    (char) ('a' + i) + "12345",
+                    i * 10, i * 100000));
+        }
         return this;
     }
 
--- src/me/day15/smartstore/groups/Group.java
+++ src/me/day15/smartstore/groups/Group.java
@@ -3,5 +3,10 @@ package me.day15.smartstore.groups;
 public class Group { // 그룹 1명 (NONE, GENERAL, VIP, VVIP)
     private GroupType groupType;
     private Parameter parameter;
+
+    public Group(GroupType groupType, Parameter parameter) {
+        this.groupType = groupType;
+        this.parameter = parameter;
+    }
 }
 
